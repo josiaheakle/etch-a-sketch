154c681c8075029e80953cba8b59bece22b7d026
Josiah Eakle <josiaheakle@gmail.com>
2020-08-28 23:15:39 -0400

    added color changer and fixed functionality

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const parentDiv   = document.querySelector('#parent-container');
 const colorDiv    = document.querySelector("#color-container")
 
 let colorChoice = "black";
+let canDraw = false;
 
 const colorPickBtn = document.createElement('button');
 colorPickBtn.style.border = "1px solid gray";
@@ -13,28 +14,115 @@ colorPickBtn.style.backgroundColor = "rgba(0, 185, 0, 0.568)";
 colorPickBtn.textContent = "Change Color"
 
 const redButton = document.createElement('button');
+redButton.id = "red";
+redButton.className = "color-button";
+redButton.style.marginLeft = "2vw";
 redButton.style.border = "1px solid gray";
-redButton.style.width = "8vw";
-redButton.style.height = "8vw";
+redButton.style.width = "8vh";
+redButton.style.height = "8vh";
 redButton.style.backgroundColor = "red";
 
+const blackButton = document.createElement('button');
+blackButton.id = "black";
+blackButton.className = "color-button";
+blackButton.style.border = "1px solid gray";
+blackButton.style.width = "8vh";
+blackButton.style.height = "8vh";
+blackButton.style.backgroundColor = "black";
 
+const grayButton = document.createElement('button');
+grayButton.id = "gray";
+grayButton.style.marginLeft = "2vw";
+grayButton.style.border = "1px solid gray";
+grayButton.style.width = "8vh";
+grayButton.style.height = "8vh";
+grayButton.style.backgroundColor = "gray";
+
+const whiteButton = document.createElement('button');
+whiteButton.id = "white";
+whiteButton.style.marginLeft = "2vw";
+whiteButton.className = "color-button";
+whiteButton.style.border = "1px solid gray";
+whiteButton.style.width = "8vh";
+whiteButton.style.height = "8vh";
+whiteButton.style.backgroundColor = "white";
+
+const greenButton = document.createElement('button');
+greenButton.id = "green";
+greenButton.style.marginLeft = "2vw";
+greenButton.className = "color-button";
+greenButton.style.border = "1px solid gray";
+greenButton.style.width = "8vh";
+greenButton.style.height = "8vh";
+greenButton.style.backgroundColor = "green";
+
+const blueButton = document.createElement('button');
+blueButton.id = "blue";
+blueButton.style.marginLeft = "2vw";
+blueButton.className = "color-button";
+blueButton.style.border = "1px solid gray";
+blueButton.style.width = "8vh";
+blueButton.style.height = "8vh";
+blueButton.style.backgroundColor = "blue";
+
+const yellowButton = document.createElement('button');
+yellowButton.id = "yellow";
+yellowButton.style.marginLeft = "2vw";
+yellowButton.className = "color-button";
+yellowButton.style.border = "1px solid gray";
+yellowButton.style.width = "8vh";
+yellowButton.style.height = "8vh";
+yellowButton.style.backgroundColor = "yellow";
+
+const pinkButton = document.createElement('button');
+pinkButton.id = "pink";
+pinkButton.style.marginLeft = "2vw";
+pinkButton.className = "color-button";
+pinkButton.style.border = "1px solid gray";
+pinkButton.style.width = "8vh";
+pinkButton.style.height = "8vh";
+pinkButton.style.backgroundColor = "pink";
+
+function removeColors() {
+    colorButtons = colorDiv.querySelectorAll('button');
+    colorButtons.forEach(element => {
+        colorDiv.removeChild(element);
+    });
+    // draw();
+}
 
 // when the player presses the color button,
 // append the color buttons to the color div
 // when new color is chosen, remove them
 function chooseColor() {
+    colorDiv.appendChild(blackButton);
+    colorDiv.appendChild(grayButton);
+    colorDiv.appendChild(whiteButton);
+    colorDiv.appendChild(redButton);
+    colorDiv.appendChild(blueButton);
+    colorDiv.appendChild(yellowButton);
+    colorDiv.appendChild(greenButton);
+    colorDiv.appendChild(pinkButton);
 
-
+    colorButtons = colorDiv.querySelectorAll('button');
+    colorButtons.forEach(element => {
+        console.log(element.id);
+        element.addEventListener('click', function() {
+            console.log(`choice: ${element.id}`);
+            colorChoice = element.id;
+            removeColors();
+        })
+    });
 
 }
 
 function draw() {
 
+    console.log('draw function called!');
 
     // add functionality to only draw when the mouse button is clicked
-    let canDraw = false;
-    drawDivs = document.querySelectorAll('.draw-div');
+    
+    const drawDivs = document.querySelectorAll('.draw-div');
 
     // if the mouse button is pressed - the user will be drawing
     parentDiv.onmousedown = function() {
@@ -45,7 +133,7 @@ function draw() {
     }
     drawDivs.forEach(element => {
         element.onmouseover = function () {
-
+            console.log(`mouse over ${element.id}, canDraw: ${canDraw}`)
             if (canDraw) {
                 element.style.backgroundColor = colorChoice;
             }
@@ -71,6 +159,8 @@ function newGrid() {
     // Show the border of the container
     parentDiv.style.border = "1px gray solid";
     resetDiv.appendChild(colorPickBtn);
+
+    colorPickBtn.addEventListener('click', chooseColor);
 
     // Remove all the old containers 
     const oldDivs = parentDiv.querySelectorAll('div');
